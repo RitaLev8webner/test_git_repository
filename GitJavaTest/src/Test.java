9c42086246116fe055ae04e6f99e872bdfe45a0b
ritaSharma <rita.sharma@webners.com>
2015-06-04 13:14:49 +0530

    Another message added

--- GitJavaTest/src/Test.java
+++ GitJavaTest/src/Test.java
@@ -3,8 +3,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  System.out.println("hello How r u");
-  System.out.println("hello I am good ");
+		System.out.println("hello How r u");
+		System.out.println("hello I am good ");
+		System.out.println("looks cool");
 	}
 
 }
