67126e32ba9b6c4ee94397018718aa7c508a4228
ritaSharma <rita.sharma@webners.com>
2015-06-04 13:02:17 +0530

    Add Message to Test

--- GitJavaTest/src/Test.java
+++ GitJavaTest/src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   System.out.println("hello How r u");
+  System.out.println("hello I am good ");
 	}
 
 }
