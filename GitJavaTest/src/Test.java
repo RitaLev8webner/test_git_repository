f5c427e979d6b92a2cfb66fbbf76b2e8b6399cac
ritaSharma <rita.sharma@webners.com>
2015-06-04 13:40:15 +0530

    log messages commented out

--- GitJavaTest/src/Test.java
+++ GitJavaTest/src/Test.java
@@ -5,7 +5,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("hello How r u");
 		System.out.println("hello I am good ");
-		System.out.println("looks cool");
+		//System.out.println("looks cool");
 		//System.out.println("good test of commit and push cheers.");
 	}
 
